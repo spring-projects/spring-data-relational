369ca6d599a0d333930f37d6408e59929145767e
Kazuki Shimizu <kazuki43zoo@gmail.com>
2018-02-09 21:15:47 +0900

    DATAJDBC-175 - Supports simple types as return type for annotated methods.
    Original pull request: #43.

--- src/main/java/org/springframework/data/jdbc/repository/support/JdbcQueryLookupStrategy.java
+++ src/main/java/org/springframework/data/jdbc/repository/support/JdbcQueryLookupStrategy.java
@@ -17,6 +17,7 @@ package org.springframework.data.jdbc.repository.support;
 
 import java.lang.reflect.Method;
 
+import org.springframework.core.convert.ConversionService;
 import org.springframework.data.jdbc.core.DataAccessStrategy;
 import org.springframework.data.jdbc.core.EntityRowMapper;
 import org.springframework.data.jdbc.mapping.model.JdbcMappingContext;
@@ -27,22 +28,26 @@ import org.springframework.data.repository.query.EvaluationContextProvider;
 import org.springframework.data.repository.query.QueryLookupStrategy;
 import org.springframework.data.repository.query.RepositoryQuery;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.SingleColumnRowMapper;
 
 /**
  * {@link QueryLookupStrategy} for JDBC repositories. Currently only supports annotated queries.
  *
  * @author Jens Schauder
+ * @author Kazuki Shimizu
  */
 class JdbcQueryLookupStrategy implements QueryLookupStrategy {
 
 	private final JdbcMappingContext context;
 	private final DataAccessStrategy accessStrategy;
+	private final ConversionService conversionService;
 
 	JdbcQueryLookupStrategy(EvaluationContextProvider evaluationContextProvider, JdbcMappingContext context,
 			DataAccessStrategy accessStrategy) {
 
 		this.context = context;
 		this.accessStrategy = accessStrategy;
+		this.conversionService = context.getConversions();
 	}
 
 	@Override
@@ -50,10 +55,12 @@ class JdbcQueryLookupStrategy implements QueryLookupStrategy {
 			ProjectionFactory projectionFactory, NamedQueries namedQueries) {
 
 		JdbcQueryMethod queryMethod = new JdbcQueryMethod(method, repositoryMetadata, projectionFactory);
-		Class<?> domainType = queryMethod.getReturnedObjectType();
-		RowMapper<?> rowMapper = new EntityRowMapper<>(context.getRequiredPersistentEntity(domainType),
-				context.getConversions(), context, accessStrategy);
-
+		Class<?> returnedObjectType = queryMethod.getReturnedObjectType();
+		RowMapper<?> rowMapper = context.getSimpleTypeHolder().isSimpleType(returnedObjectType)
+				? SingleColumnRowMapper.newInstance(returnedObjectType, conversionService)
+				: new EntityRowMapper<>(context.getRequiredPersistentEntity(returnedObjectType), conversionService,
+						context, accessStrategy);
 		return new JdbcRepositoryQuery(queryMethod, context, rowMapper);
 	}
+
 }
