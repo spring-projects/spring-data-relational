c59e9a58eb979dc4d2a8721469dffe057648752a
Mark Paluch <mpaluch@pivotal.io>
2018-06-04 16:03:34 +0200

    #2 - Add DatabaseClient.
    Provide DatabaseClient and add ExternalDatabase class to provide database connection details/encapsulate a dockerized testcontainer.

---
+++ src/main/java/org/springframework/data/jdbc/core/function/DefaultReactiveDataAccessStrategy.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.jdbc.core.function;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+
+import org.springframework.data.convert.EntityInstantiators;
+import org.springframework.data.jdbc.core.mapping.JdbcMappingContext;
+import org.springframework.data.jdbc.core.mapping.JdbcPersistentEntity;
+import org.springframework.data.jdbc.core.mapping.JdbcPersistentProperty;
+import org.springframework.data.mapping.PersistentPropertyAccessor;
+import org.springframework.data.util.Pair;
+import org.springframework.util.ClassUtils;
+
+import io.r2dbc.spi.Row;
+import io.r2dbc.spi.RowMetadata;
+
+/**
+ * @author Mark Paluch
+ */
+public class DefaultReactiveDataAccessStrategy implements ReactiveDataAccessStrategy {
+
+	private final EntityInstantiators instantiators = new EntityInstantiators();
+	private final JdbcMappingContext mappingContext = new JdbcMappingContext();
+
+	@Override
+	public List<Pair<String, Object>> getInsert(Object object) {
+
+		Class<?> userClass = ClassUtils.getUserClass(object);
+
+		JdbcPersistentEntity<?> entity = mappingContext.getRequiredPersistentEntity(userClass);
+		PersistentPropertyAccessor propertyAccessor = entity.getPropertyAccessor(object);
+
+		List<Pair<String, Object>> values = new ArrayList<>();
+
+		for (JdbcPersistentProperty property : entity) {
+
+			Object value = propertyAccessor.getProperty(property);
+
+			if (value == null) {
+				continue;
+			}
+
+			values.add(Pair.of(property.getColumnName(), value));
+		}
+
+		return values;
+	}
+
+	@Override
+	public <T> BiFunction<Row, RowMetadata, T> getRowMapper(Class<T> typeToRead) {
+		return new EntityRowMapper<T>((JdbcPersistentEntity) mappingContext.getRequiredPersistentEntity(typeToRead),
+				instantiators, mappingContext);
+	}
+
+	@Override
+	public String getTableName(Class<?> type) {
+		return mappingContext.getRequiredPersistentEntity(type).getTableName();
+	}
+}
---
+++ src/test/java/org/springframework/data/jdbc/core/function/ExternalDatabase.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.jdbc.core.function;
+
+import lombok.Builder;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.AssumptionViolatedException;
+import org.junit.rules.ExternalResource;
+
+/**
+ * {@link ExternalResource} wrapper to encapsulate {@link ProvidedDatabase} and
+ * {@link org.testcontainers.containers.PostgreSQLContainer}.
+ *
+ * @author Mark Paluch
+ */
+public abstract class ExternalDatabase extends ExternalResource {
+
+	/**
+	 * @return the post of the database service.
+	 */
+	public abstract int getPort();
+
+	/**
+	 * @return hostname on which the database service runs.
+	 */
+	public abstract String getHostname();
+
+	/**
+	 * @return name of the database.
+	 */
+	public abstract String getDatabase();
+
+	/**
+	 * @return database user name.
+	 */
+	public abstract String getUsername();
+
+	@Override
+	protected void before() {
+
+		try (Socket socket = new Socket()) {
+			;
+			socket.connect(new InetSocketAddress(getHostname(), getPort()), Math.toIntExact(TimeUnit.SECONDS.toMillis(5)));
+
+		} catch (IOException e) {
+			throw new AssumptionViolatedException(
+					String.format("Cannot connect to %s:%d. Skiping tests.", getHostname(), getPort()));
+		}
+	}
+
+	/**
+	 * @return password for the database user.
+	 */
+	public abstract String getPassword();
+
+	/**
+	 * Provided (unmanaged resource) database connection coordinates.
+	 */
+	@Builder
+	static class ProvidedDatabase extends ExternalDatabase {
+
+		private final int port;
+		private final String hostname;
+		private final String database;
+		private final String username;
+		private final String password;
+
+		/* (non-Javadoc)
+		 * @see org.springframework.data.jdbc.core.function.ExternalDatabase#getPort()
+		 */
+		@Override
+		public int getPort() {
+			return port;
+		}
+
+		/* (non-Javadoc)
+		 * @see org.springframework.data.jdbc.core.function.ExternalDatabase#getHostname()
+		 */
+		@Override
+		public String getHostname() {
+			return hostname;
+		}
+
+		/* (non-Javadoc)
+		 * @see org.springframework.data.jdbc.core.function.ExternalDatabase#getDatabase()
+		 */
+		@Override
+		public String getDatabase() {
+			return database;
+		}
+
+		/* (non-Javadoc)
+		 * @see org.springframework.data.jdbc.core.function.ExternalDatabase#getUsername()
+		 */
+		@Override
+		public String getUsername() {
+			return username;
+		}
+
+		/* (non-Javadoc)
+		 * @see org.springframework.data.jdbc.core.function.ExternalDatabase#getPassword()
+		 */
+		@Override
+		public String getPassword() {
+			return password;
+		}
+	}
+}
