93dfc4848fd7258e2d3dabdd7181c9c4126e3cbd
Toshiaki Maki <tmaki@pivotal.io>
2018-07-01 03:41:25 +0900

    DATAJDBC-229 Add java.util.UUID as a custom simple type

--- src/test/java/org/springframework/data/relational/core/mapping/RelationalMappingContextUnitTests.java
+++ src/test/java/org/springframework/data/relational/core/mapping/RelationalMappingContextUnitTests.java
@@ -15,19 +15,21 @@
  */
 package org.springframework.data.relational.core.mapping;
 
-import static org.assertj.core.api.Assertions.*;
+import org.junit.Test;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mapping.PropertyPath;
 
 import java.util.List;
+import java.util.UUID;
 
-import org.junit.Test;
-import org.springframework.data.mapping.PropertyPath;
-import org.springframework.data.relational.core.mapping.RelationalMappingContext;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Unit tests for {@link RelationalMappingContext}.
  *
  * @author Jens Schauder
  * @author Oliver Gierke
+ * @author Toshiaki Maki
  */
 public class RelationalMappingContextUnitTests {
 
@@ -55,6 +57,14 @@ public class RelationalMappingContextUnitTests {
 				.containsExactly("one.two", "one");
 	}
 
+    @Test // DATAJDBC-229
+    public void uuidPropertyIsNotEntity() {
+        RelationalMappingContext mappingContext = new RelationalMappingContext();
+        RelationalPersistentEntity<?> entity = mappingContext.getPersistentEntity(EntityWithUuid.class);
+        RelationalPersistentProperty uuidProperty = entity.getRequiredPersistentProperty("uuid");
+        assertThat(uuidProperty.isEntity()).isFalse();
+    }
+
 	static class DummyEntity {
 
 		String simpleProperty;
@@ -67,5 +77,10 @@ public class RelationalMappingContextUnitTests {
 
 	static class LevelTwo {
 		String someValue;
+	}
+
+	static class EntityWithUuid {
+		@Id
+		UUID uuid;
 	}
 }
