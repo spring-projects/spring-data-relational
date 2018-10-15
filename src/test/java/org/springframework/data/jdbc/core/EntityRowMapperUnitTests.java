aec57220276ad334995a708bf5fc1be6b4164b0d
Jens Schauder <jschauder@pivotal.io>
2018-10-15 16:12:21 +0200

    DATAJDBC-273 - Added support for collections and similar in constructors of entities.
    Instance creation now uses the same code for materializing property values as the part setting properties.

--- src/test/java/org/springframework/data/jdbc/core/EntityRowMapperUnitTests.java
+++ src/test/java/org/springframework/data/jdbc/core/EntityRowMapperUnitTests.java
@@ -21,6 +21,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Wither;
 
@@ -201,6 +202,18 @@ public class EntityRowMapperUnitTests {
 				.isEqualTo(new String[] { "111", "222", "333" });
 	}
 
+	@Test // DATAJDBC-273
+	public void handlesNonSimplePropertyInConstructor() throws SQLException {
+
+		ResultSet rs = mockResultSet(asList("id"), //
+				ID_FOR_ENTITY_REFERENCING_LIST);
+		rs.next();
+
+		EntityWithListInConstructor extracted = createRowMapper(EntityWithListInConstructor.class).mapRow(rs, 1);
+
+		assertThat(extracted.content).hasSize(2);
+	}
+
 	private <T> EntityRowMapper<T> createRowMapper(Class<T> type) {
 		return createRowMapper(type, NamingStrategy.INSTANCE);
 	}
@@ -246,7 +259,7 @@ public class EntityRowMapperUnitTests {
 								"Number of values [%d] must be a multiple of the number of columns [%d]", //
 								values.length, //
 								columns.size() //
-				) //
+						) //
 		);
 
 		List<Map<String, Object>> result = convertValues(columns, values);
@@ -318,12 +331,13 @@ public class EntityRowMapperUnitTests {
 			return index < 0 && !values.isEmpty();
 		}
 
-		private Object getObject(String column) {
+		private Object getObject(String column) throws SQLException {
 
 			Map<String, Object> rowMap = values.get(index);
 
-			Assert.isTrue(rowMap.containsKey(column),
-					String.format("Trying to access a column (%s) that does not exist", column));
+			if (!rowMap.containsKey(column)) {
+				throw new SQLException(String.format("Trying to access a column (%s) that does not exist", column));
+			}
 
 			return rowMap.get(column);
 		}
@@ -407,5 +421,13 @@ public class EntityRowMapperUnitTests {
 		MixedProperties withThree(String three) {
 			return new MixedProperties(one, two, three);
 		}
+	}
+
+	@AllArgsConstructor
+	static class EntityWithListInConstructor {
+
+		@Id final Long id;
+
+		final List<Trivial> content;
 	}
 }
