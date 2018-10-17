7c56af66329120fa2800a158ae3493f87fd14717
Mark Paluch <mpaluch@pivotal.io>
2018-10-17 14:43:05 +0200

    DATAJDBC-272 - Polishing.
    Add missing relational.repository package that got lost during module split.

---
+++ spring-data-relational/src/main/java/org/springframework/data/relational/repository/support/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Support infrastructure for query derivation of relational database repositories.
+ */
+@NonNullApi
+package org.springframework.data.relational.repository.support;
+
+import org.springframework.lang.NonNullApi;
