2c0af465536c4fdd1129533b8b69fae984a4828b
Thomas Lang <thomas.lang@th-deg.de>
2018-10-30 08:55:22 +0100

    DATAJDBC-258 - some polishing done after rebasing.

--- src/test/resources/org.springframework.data.jdbc.repository/JdbcRepositoryWithCollectionsNoIdIntegrationTests-mssql.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS dummy_entity;
-DROP TABLE IF EXISTS element;
-CREATE TABLE dummy_entity ( id BIGINT identity PRIMARY KEY, NAME VARCHAR(100));
-CREATE TABLE element (content VARCHAR(100), dummy_entity BIGINT);
