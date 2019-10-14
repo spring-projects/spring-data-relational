a066d2f14ce7d6a082d55ced318b2f624ec59e35
Jens Schauder <jschauder@pivotal.io>
2019-10-14 16:09:35 +0200

    DATAJDBC-431 - ReadOnlyProperty now no longer written.
    The problem was that the SqlGenerator honored the annotation but they were included as query parameters and therefore automatically added back again.
    
    Also:
    
    * Simplified the relevant filter in the SqlGenerator.
    * Introduced a meta annotation for running tests only agains HsqlDb.
    
    Original pull request: #175.

---
+++ spring-data-jdbc/src/test/java/org/springframework/data/jdbc/testing/HsqlDbOnly.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.jdbc.testing;
+
+import org.springframework.test.annotation.IfProfileValue;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Run the annotated test only against a HsqlDb database.
+ *
+ * Requires the use of
+ *
+ * @author Jens Schauder
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@IfProfileValue(name = "current.database.is.not.hsqldb", value = "false")
+public @interface HsqlDbOnly {
+}
