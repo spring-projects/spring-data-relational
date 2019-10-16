6e715cb6b3c87c722901cd2cc791fa8511c111e6
Jens Schauder <jschauder@pivotal.io>
2019-10-16 14:32:17 +0200

    DATAJDBC-427 - Polishing.
    Used isEqual assertions instead of directly calling equals.
    Code formatting.

--- spring-data-jdbc/src/test/java/org/springframework/data/jdbc/core/mapping/IdOnlyAggregateReferenceTest.java
+++ spring-data-jdbc/src/test/java/org/springframework/data/jdbc/core/mapping/IdOnlyAggregateReferenceTest.java
@@ -11,30 +11,37 @@ import org.springframework.data.jdbc.core.mapping.AggregateReference.IdOnlyAggre
  * @author Myeonghyeon Lee
  */
 public class IdOnlyAggregateReferenceTest {
-    @Test   // DATAJDBC-427
-    public void equals() {
-        AggregateReference<DummyEntity, String> reference1 = AggregateReference.to("1");
-        AggregateReference<DummyEntity, String> reference2 = AggregateReference.to("1");
-        assertThat(reference1.equals(reference2)).isTrue();
-        assertThat(reference2.equals(reference1)).isTrue();
-    }
 
-    @Test   // DATAJDBC-427
-    public void equalsFalse() {
-        AggregateReference<DummyEntity, String> reference1 = AggregateReference.to("1");
-        AggregateReference<DummyEntity, String> reference2 = AggregateReference.to("2");
-        assertThat(reference1.equals(reference2)).isFalse();
-        assertThat(reference2.equals(reference1)).isFalse();
-    }
+	@Test // DATAJDBC-427
+	public void equals() {
 
-    @Test   // DATAJDBC-427
-    public void hashCodeTest() {
-        AggregateReference<DummyEntity, String> reference1 = AggregateReference.to("1");
-        AggregateReference<DummyEntity, String> reference2 = AggregateReference.to("1");
-        assertThat(reference1.hashCode()).isEqualTo(reference2.hashCode());
-    }
+		AggregateReference<DummyEntity, String> reference1 = AggregateReference.to("1");
+		AggregateReference<DummyEntity, String> reference2 = AggregateReference.to("1");
 
-    private static class DummyEntity {
-        private String id;
-    }
+		assertThat(reference1).isEqualTo(reference2);
+		assertThat(reference2).isEqualTo(reference1);
+	}
+
+	@Test // DATAJDBC-427
+	public void equalsFalse() {
+
+		AggregateReference<DummyEntity, String> reference1 = AggregateReference.to("1");
+		AggregateReference<DummyEntity, String> reference2 = AggregateReference.to("2");
+
+		assertThat(reference1).isNotEqualTo(reference2);
+		assertThat(reference2).isNotEqualTo(reference1);
+	}
+
+	@Test // DATAJDBC-427
+	public void hashCodeTest() {
+
+		AggregateReference<DummyEntity, String> reference1 = AggregateReference.to("1");
+		AggregateReference<DummyEntity, String> reference2 = AggregateReference.to("1");
+
+		assertThat(reference1.hashCode()).isEqualTo(reference2.hashCode());
+	}
+
+	private static class DummyEntity {
+		private String id;
+	}
 }
