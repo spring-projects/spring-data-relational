20a3b854566f16d1af4aa1f8f5b9e9b0b5c1e16d
Jens Schauder <jschauder@pivotal.io>
2020-05-06 08:30:26 +0200

    DATAJDBC-257 - Changes based on code review.
    Removed superfluous commented code and imports.
    Fixed year in copyright blocks.
    Improved Javadoc comments.

--- spring-data-jdbc/src/test/java/org/springframework/data/jdbc/testing/Db2DataSourceConfiguration.java
+++ spring-data-jdbc/src/test/java/org/springframework/data/jdbc/testing/Db2DataSourceConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 the original author or authors.
+ * Copyright 2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,7 +24,7 @@ import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.testcontainers.containers.Db2Container;
 
 /**
- * {@link DataSource} setup for HSQL.
+ * {@link DataSource} setup for DB2.
  *
  * @author Jens Schauder
  * @author Oliver Gierke
--- spring-data-relational/src/main/java/org/springframework/data/relational/core/dialect/Db2Dialect.java
+++ spring-data-relational/src/main/java/org/springframework/data/relational/core/dialect/Db2Dialect.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,13 +15,7 @@
  */
 package org.springframework.data.relational.core.dialect;
 
-import lombok.RequiredArgsConstructor;
-
 import org.springframework.data.relational.core.sql.IdentifierProcessing;
-import org.springframework.data.relational.core.sql.IdentifierProcessing.LetterCasing;
-import org.springframework.data.relational.core.sql.IdentifierProcessing.Quoting;
-import org.springframework.util.Assert;
-import org.springframework.util.ClassUtils;
 
 /**
  * An SQL dialect for DB2.
