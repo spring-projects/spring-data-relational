2efcda2a911f27599b7d9c21bf510816d8c2fa57
Jens Schauder <jschauder@vmware.com>
2020-12-01 11:18:41 +0100

    DATAJDBC-637 - Time conversion now preserve nanosecond precision.
    The standard JSR 310 converters are no longer used for conversions between java.util.Date and java.time.*.
    New converters based converting to/from Timestamp are used.
    
    This preserves the precision because both the java.time.* API and Timestamp have nanosecond precision, while java.util.Date has not.
    
    Original pull request: #254.

--- spring-data-jdbc/src/main/java/org/springframework/data/jdbc/core/convert/BasicJdbcConverter.java
+++ spring-data-jdbc/src/main/java/org/springframework/data/jdbc/core/convert/BasicJdbcConverter.java
@@ -93,7 +93,7 @@ public class BasicJdbcConverter extends BasicRelationalConverter implements Jdbc
 			MappingContext<? extends RelationalPersistentEntity<?>, ? extends RelationalPersistentProperty> context,
 			RelationResolver relationResolver) {
 
-		super(context);
+		super(context, new JdbcCustomConversions());
 
 		Assert.notNull(relationResolver, "RelationResolver must not be null");
 
