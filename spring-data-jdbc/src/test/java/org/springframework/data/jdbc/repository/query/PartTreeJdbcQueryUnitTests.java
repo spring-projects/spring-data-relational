935d528e855190776c96cb0209681a59359dcbe3
Charles <charleszkq@gmail.com>
2021-11-21 19:17:46 -0600

    fix flaky test

--- spring-data-jdbc/src/test/java/org/springframework/data/jdbc/repository/query/PartTreeJdbcQueryUnitTests.java
+++ spring-data-jdbc/src/test/java/org/springframework/data/jdbc/repository/query/PartTreeJdbcQueryUnitTests.java
@@ -589,10 +589,12 @@ public class PartTreeJdbcQueryUnitTests {
 				new Object[] { new Address("Hello", "World") });
 		ParametrizedQuery query = jdbcQuery.createQuery(accessor, returnedType);
 
-		String expectedSql = BASE_SELECT + " WHERE (" + TABLE + ".\"USER_STREET\" = :user_street AND " + TABLE
+		String expectedSql1 = BASE_SELECT + " WHERE (" + TABLE + ".\"USER_STREET\" = :user_street AND " + TABLE
 				+ ".\"USER_CITY\" = :user_city)";
+		String expectedSql2 = BASE_SELECT + " WHERE (" + TABLE + ".\"USER_CITY\" = :user_city AND " + TABLE
+				+ ".\"USER_STREET\" = :user_street)";
 
-		assertThat(query.getQuery()).isEqualTo(expectedSql);
+		assertThat(query.getQuery().equals(expectedSql1) || query.getQuery().equals(expectedSql2)).isTrue();
 		assertThat(query.getParameterSource().getValue("user_street")).isEqualTo("Hello");
 		assertThat(query.getParameterSource().getValue("user_city")).isEqualTo("World");
 	}
