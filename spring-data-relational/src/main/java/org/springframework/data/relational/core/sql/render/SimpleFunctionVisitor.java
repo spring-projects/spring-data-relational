a132d432e8de9fc108bc53f4065cab09c7d9a1a2
Jens Schauder <jschauder@vmware.com>
2022-02-02 15:29:57 +0100

    Render windowing functions without arguments correctly.
    Closes #1153
    See #1019

--- spring-data-relational/src/main/java/org/springframework/data/relational/core/sql/render/SimpleFunctionVisitor.java
+++ spring-data-relational/src/main/java/org/springframework/data/relational/core/sql/render/SimpleFunctionVisitor.java
@@ -30,7 +30,6 @@ class SimpleFunctionVisitor extends TypedSingleConditionRenderSupport<SimpleFunc
 
 	private final StringBuilder part = new StringBuilder();
 	private boolean needsComma = false;
-	private String functionName;
 
 	SimpleFunctionVisitor(RenderContext context) {
 		super(context);
@@ -49,9 +48,6 @@ class SimpleFunctionVisitor extends TypedSingleConditionRenderSupport<SimpleFunc
 				part.append(", ");
 			}
 
-			if (part.length() == 0) {
-				part.append(functionName).append("(");
-			}
 			part.append(consumeRenderedPart());
 			needsComma = true;
 		}
@@ -66,7 +62,8 @@ class SimpleFunctionVisitor extends TypedSingleConditionRenderSupport<SimpleFunc
 	@Override
 	Delegation enterMatched(SimpleFunction segment) {
 
-		functionName = segment.getFunctionName();
+		part.append(segment.getFunctionName()).append("(");
+
 		return super.enterMatched(segment);
 	}
 
