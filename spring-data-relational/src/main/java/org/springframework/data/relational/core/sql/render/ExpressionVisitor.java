d5f7ab56c2483fc6ed05ee9075403bef46add519
schauder <jschauder@vmware.com>
2022-10-19 14:32:08 +0200

    Fix broken join conditions with InlineQuery.
    Before this fix, whenever a column of an inline query was rendered the `InlineQuery` and all its children were visited, resulting in spurious output.
    This is no prevented by injecting a NoopVisitor.
    
    Closes: #1362
    Original pull request: #1368

--- spring-data-relational/src/main/java/org/springframework/data/relational/core/sql/render/ExpressionVisitor.java
+++ spring-data-relational/src/main/java/org/springframework/data/relational/core/sql/render/ExpressionVisitor.java
@@ -123,6 +123,11 @@ class ExpressionVisitor extends TypedSubtreeVisitor<Expression> implements PartR
 			return Delegation.delegateTo(visitor);
 		}
 
+		if (segment instanceof InlineQuery) {
+
+			NoopVisitor<InlineQuery> partRenderer = new NoopVisitor(InlineQuery.class);
+			return Delegation.delegateTo(partRenderer);
+		}
 		return super.enterNested(segment);
 	}
 
