8245f5b44d9655ace44f33e788078b14d359ddcc
Viktor Ardelean <viktor0710@gmail.com>
2022-11-02 15:38:41 +0200

    Improve error message when required field is missing in SqlIdentifier::getReference

--- spring-data-relational/src/main/java/org/springframework/data/relational/core/sql/SqlIdentifier.java
+++ spring-data-relational/src/main/java/org/springframework/data/relational/core/sql/SqlIdentifier.java
@@ -65,7 +65,7 @@ public interface SqlIdentifier extends Streamable<SqlIdentifier> {
 
 		@Override
 		public String getReference(IdentifierProcessing processing) {
-			throw new UnsupportedOperationException("An empty SqlIdentifier can't be used in to create column names");
+			throw new UnsupportedOperationException("An empty SqlIdentifier can't be used in to create column names. Ensure that all selected fields are present in the aggregation clause.");
 		}
 
 		public String toString() {
