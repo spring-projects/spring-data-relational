8703653453d1dbb43a657d1aaf485b16eb42b320
Greg L. Turnquist <gturnquist@vmware.com>
2023-05-16 09:20:59 -0500

    Use Harbor Proxy service on CI.
    When run on CI servers, leverage an internal proxy service using Testcontainers ability to plugin a custom ImageNameSubstitor.
    
    See #1518
    Original Pull Request: #1516

--- ci/test.sh
+++ ci/test.sh
@@ -3,6 +3,11 @@
 set -euo pipefail
 
 ci/accept-third-party-license.sh
+
+echo "Copying ProxyImageNameSubstitutor into JDBC and R2DBC..."
+cp spring-data-relational/src/test/java/org/springframework/data/ProxyImageNameSubstitutor.java spring-data-jdbc/src/test/java/org/springframework/data
+cp spring-data-relational/src/test/java/org/springframework/data/ProxyImageNameSubstitutor.java spring-data-r2dbc/src/test/java/org/springframework/data
+
 mkdir -p /tmp/jenkins-home
 chown -R 1001:1001 .
 MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home" \
