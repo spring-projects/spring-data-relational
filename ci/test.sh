16ab1c484263eac1a49ee9be5d9f388f7a37b492
Jens Schauder <jschauder@vmware.com>
2023-10-11 15:12:39 +0200

    Using verify during the CI build

--- ci/test.sh
+++ ci/test.sh
@@ -12,4 +12,4 @@ mkdir -p /tmp/jenkins-home
 chown -R 1001:1001 .
 MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home" \
   ./mvnw -s settings.xml \
-  -P${PROFILE} clean dependency:list test -Dsort -U -B -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-jdbc
+  -P${PROFILE} clean dependency:list verify -Dsort -U -B -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-jdbc
