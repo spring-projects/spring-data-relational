90ee50b8679f46a1051df67695e907fb8bca62c9
Greg L. Turnquist <gturnquist@vmware.com>
2023-10-19 10:48:34 -0500

    Polishing.
    See #1645

--- ci/clean.sh
+++ ci/clean.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home" \
+MAVEN_OPTS="-Duser.name=spring-builds+jenkins -Duser.home=/tmp/jenkins-home" \
   ./mvnw -s settings.xml clean -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-jdbc
