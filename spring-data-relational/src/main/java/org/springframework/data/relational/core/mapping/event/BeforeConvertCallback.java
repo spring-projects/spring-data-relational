b0eecc3fb9e0b62cd996ac0e199f6197330ba49a
Jens Schauder <jschauder@vmware.com>
2023-11-21 11:20:29 +0100

    Fix documentation of BeforeConvertCallback.
    Closes #1673

--- spring-data-relational/src/main/java/org/springframework/data/relational/core/mapping/event/BeforeConvertCallback.java
+++ spring-data-relational/src/main/java/org/springframework/data/relational/core/mapping/event/BeforeConvertCallback.java
@@ -19,7 +19,7 @@ import org.springframework.data.mapping.callback.EntityCallback;
 
 /**
  * An {@link EntityCallback} that gets invoked before the aggregate is converted into a database change. The decision if
- * the change will be an insert or update is made after this callback gets called.
+ * the change will be an insert or update is made before this callback gets called.
  * <p>
  * This is the correct callback if you want to create Id values for new aggregates.
  * <p>
