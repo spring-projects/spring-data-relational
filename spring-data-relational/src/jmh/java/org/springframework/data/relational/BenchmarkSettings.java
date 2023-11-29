060bc2f30bbbe5a7b8ad0dc5fe2f11350f93cc4a
Mark Paluch <mpaluch@vmware.com>
2023-11-29 09:48:15 +0100

    Introduce AggregatePath benchmark.
    See #1679

--- spring-data-relational/src/jmh/java/org/springframework/data/relational/BenchmarkSettings.java
+++ spring-data-relational/src/jmh/java/org/springframework/data/relational/BenchmarkSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2022 the original author or authors.
+ * Copyright 2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.data.relational.core.sqlgeneration;
+package org.springframework.data.relational;
 
 import java.util.concurrent.TimeUnit;
 
