967f0f9179e2c8cbdbfbfd2b633362c6b532379b
Mark Paluch <mark.paluch@broadcom.com>
2024-05-23 08:48:24 +0200

    Upgrade to SQL Server 2022-latest.
    See #1768

--- ci/accept-third-party-license.sh
+++ ci/accept-third-party-license.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 {
-  echo "mcr.microsoft.com/mssql/server:2022-CU5-ubuntu-20.04"
+  echo "mcr.microsoft.com/mssql/server:2022-latest"
   echo "ibmcom/db2:11.5.7.0a"
-  echo "harbor-repo.vmware.com/mcr-proxy-cache/mssql/server:2019-CU16-ubuntu-20.04"
-  echo "harbor-repo.vmware.com/mcr-proxy-cache/mssql/server:2022-CU5-ubuntu-20.04"
+  echo "harbor-repo.vmware.com/mcr-proxy-cache/mssql/server:2022-latest"
   echo "harbor-repo.vmware.com/dockerhub-proxy-cache/ibmcom/db2:11.5.7.0a"
 } > spring-data-jdbc/src/test/resources/container-license-acceptance.txt
 
@@ -12,6 +11,5 @@
   echo "mcr.microsoft.com/mssql/server:2022-latest"
   echo "ibmcom/db2:11.5.7.0a"
   echo "harbor-repo.vmware.com/mcr-proxy-cache/mssql/server:2022-latest"
-  echo "harbor-repo.vmware.com/mcr-proxy-cache/mssql/server:2022-CU5-ubuntu-20.04"
   echo "harbor-repo.vmware.com/dockerhub-proxy-cache/ibmcom/db2:11.5.7.0a"
 } > spring-data-r2dbc/src/test/resources/container-license-acceptance.txt
