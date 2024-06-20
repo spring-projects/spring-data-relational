454862c7807cb9cccc0c73e5330636042decd5f5
Mark Paluch <mark.paluch@broadcom.com>
2024-06-20 14:26:03 +0200

    Switch to Broadcom docker proxy.
    Closes #1822

--- ci/accept-third-party-license.sh
+++ ci/accept-third-party-license.sh
@@ -4,14 +4,14 @@
   echo "mcr.microsoft.com/mssql/server:2022-CU5-ubuntu-20.04"
   echo "ibmcom/db2:11.5.7.0a"
   echo "harbor-repo.vmware.com/mcr-proxy-cache/mssql/server:2019-CU16-ubuntu-20.04"
-  echo "harbor-repo.vmware.com/mcr-proxy-cache/mssql/server:2022-CU5-ubuntu-20.04"
-  echo "harbor-repo.vmware.com/dockerhub-proxy-cache/ibmcom/db2:11.5.7.0a"
+  echo "docker-hub.usw1.packages.broadcom.com/mssql/server:2022-CU5-ubuntu-20.04"
+  echo "docker-hub.usw1.packages.broadcom.com/ibmcom/db2:11.5.7.0a"
 } > spring-data-jdbc/src/test/resources/container-license-acceptance.txt
 
 {
   echo "mcr.microsoft.com/mssql/server:2022-latest"
   echo "ibmcom/db2:11.5.7.0a"
-  echo "harbor-repo.vmware.com/mcr-proxy-cache/mssql/server:2022-latest"
-  echo "harbor-repo.vmware.com/mcr-proxy-cache/mssql/server:2022-CU5-ubuntu-20.04"
-  echo "harbor-repo.vmware.com/dockerhub-proxy-cache/ibmcom/db2:11.5.7.0a"
+  echo "docker-hub.usw1.packages.broadcom.com/mssql/server:2022-latest"
+  echo "docker-hub.usw1.packages.broadcom.com/mssql/server:2022-CU5-ubuntu-20.04"
+  echo "docker-hub.usw1.packages.broadcom.com/ibmcom/db2:11.5.7.0a"
 } > spring-data-r2dbc/src/test/resources/container-license-acceptance.txt
