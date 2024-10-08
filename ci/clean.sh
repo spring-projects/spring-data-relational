ef2779ec836f0dce8fa97bdddfb1fc6c4634bec4
Mark Paluch <mark.paluch@broadcom.com>
2024-10-08 11:22:39 +0200

    Consistently run all CI steps with the same user.
    See #1883

--- ci/clean.sh
+++ ci/clean.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 export JENKINS_USER=${JENKINS_USER_NAME}
 
 MAVEN_OPTS="-Duser.name=${JENKINS_USER} -Duser.home=/tmp/jenkins-home" \
-  ./mvnw -s settings.xml -Dscan=false clean -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-jdbc
+  ./mvnw -s settings.xml -Dscan=false clean -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-jdbc -Ddevelocity.storage.directory=/tmp/jenkins-home/.develocity-root
