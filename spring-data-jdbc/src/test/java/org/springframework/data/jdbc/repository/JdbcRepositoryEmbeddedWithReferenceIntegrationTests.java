3c58acda5d1e14c42427e27601f035122fba42d6
Jens Schauder <jens.schauder@broadcom.com>
2024-11-12 09:26:12 +0100

    second test fails

--- spring-data-jdbc/src/test/java/org/springframework/data/jdbc/repository/JdbcRepositoryEmbeddedWithReferenceIntegrationTests.java
+++ spring-data-jdbc/src/test/java/org/springframework/data/jdbc/repository/JdbcRepositoryEmbeddedWithReferenceIntegrationTests.java
@@ -15,11 +15,10 @@
  */
 package org.springframework.data.jdbc.repository;
 
-import static java.util.Arrays.*;
-import static org.assertj.core.api.Assertions.*;
-
 import java.util.List;
 import java.util.Map;
+
+import javax.sql.DataSource;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,10 +33,12 @@ import org.springframework.data.relational.core.dialect.Dialect;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Embedded;
 import org.springframework.data.relational.core.mapping.Embedded.OnEmpty;
-import org.springframework.data.relational.core.sql.SqlIdentifier;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.test.jdbc.JdbcTestUtils;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
 /**
  * Very simple use cases for creation and usage of JdbcRepositories with test {@link Embedded} annotation in Entities.
@@ -51,6 +52,8 @@ public class JdbcRepositoryEmbeddedWithReferenceIntegrationTests {
 	@Autowired NamedParameterJdbcTemplate template;
 	@Autowired DummyEntityRepository repository;
 	@Autowired Dialect dialect;
+
+	@Autowired DataSource dataSource;
 
 	private static DummyEntity createDummyEntity() {
 
@@ -71,183 +74,25 @@ public class JdbcRepositoryEmbeddedWithReferenceIntegrationTests {
 	}
 
 	@Test
-	void insertJdbcTemplate() {
+	void batchInsertJdbcTemplate() {
 		Map<String, ?> params = Map.of("id", 4711, "test", "text1");
-		template.update("INSERT INTO \"DUMMY_ENTITY2\" (\"ID\", \"TEST\") VALUES (:id, :test)", params);
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		SqlParameterSource[] sqlParameterSource = new SqlParameterSource[] { new MapSqlParameterSource(params) };
+		template.batchUpdate("INSERT INTO \"DUMMY_ENTITY2\" (\"ID\", \"TEST\") VALUES (:id, :test)", sqlParameterSource,
+				keyHolder);
 	}
 
 	@Test
-	void batchInsertJdbcTemplate() {
+	void batchInsertJdbcTemplate2() {
 		Map<String, ?> params = Map.of("id", 4711, "test", "text1");
-		Map<String, ?>[] batchParams = new Map[] {params};
-		template.batchUpdate("INSERT INTO \"DUMMY_ENTITY2\" (\"ID\", \"TEST\") VALUES (:id, :test)", batchParams);
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		SqlParameterSource[] sqlParameterSources = new SqlParameterSource[] { new MapSqlParameterSource(params) };
+		template.batchUpdate("INSERT INTO \"DUMMY_ENTITY2\" (\"ID\", \"TEST\") VALUES (:id, :test)", sqlParameterSources,
+				keyHolder);
 	}
 
-	@Test // DATAJDBC-111
-	public void savesAnEntity() {
 
-		DummyEntity entity = repository.save(createDummyEntity());
 
-		assertThat(countRowsInTable("dummy_entity", entity.getId())).isEqualTo(1);
-		assertThat(countRowsInTable("dummy_entity2", entity.getId())).isEqualTo(1);
-	}
-
-	private int countRowsInTable(String name, long idValue) {
-
-		SqlIdentifier id = SqlIdentifier.quoted("ID");
-		String whereClause = id.toSql(dialect.getIdentifierProcessing()) + " = " + idValue;
-
-		return JdbcTestUtils.countRowsInTableWhere(template.getJdbcOperations(), name, whereClause);
-	}
-
-	@Test // DATAJDBC-111
-	public void saveAndLoadAnEntity() {
-
-		DummyEntity entity = repository.save(createDummyEntity());
-
-		assertThat(repository.findById(entity.getId())).hasValueSatisfying(it -> {
-			assertThat(it.getId()).isEqualTo(entity.getId());
-			assertThat(it.getEmbeddable().getTest()).isEqualTo(entity.getEmbeddable().getTest());
-			assertThat(it.getEmbeddable().getDummyEntity2().getTest())
-					.isEqualTo(entity.getEmbeddable().getDummyEntity2().getTest());
-		});
-	}
-
-	@Test // DATAJDBC-111
-	public void findAllFindsAllEntities() {
-
-		DummyEntity entity = repository.save(createDummyEntity());
-		DummyEntity other = repository.save(createDummyEntity());
-
-		Iterable<DummyEntity> all = repository.findAll();
-
-		assertThat(all)//
-				.extracting(DummyEntity::getId)//
-				.containsExactlyInAnyOrder(entity.getId(), other.getId());
-	}
-
-	@Test // DATAJDBC-111
-	public void findByIdReturnsEmptyWhenNoneFound() {
-
-		// NOT saving anything, so DB is empty
-		assertThat(repository.findById(-1L)).isEmpty();
-	}
-
-	@Test // DATAJDBC-111
-	public void update() {
-
-		DummyEntity entity = repository.save(createDummyEntity());
-
-		entity.getEmbeddable().setTest("something else");
-		entity.getEmbeddable().getDummyEntity2().setTest("another");
-		DummyEntity saved = repository.save(entity);
-
-		assertThat(repository.findById(entity.getId())).hasValueSatisfying(it -> {
-			assertThat(it.getEmbeddable().getTest()).isEqualTo(saved.getEmbeddable().getTest());
-			assertThat(it.getEmbeddable().getDummyEntity2().getTest())
-					.isEqualTo(saved.getEmbeddable().getDummyEntity2().getTest());
-		});
-	}
-
-	@Test // DATAJDBC-111
-	public void updateMany() {
-
-		DummyEntity entity = repository.save(createDummyEntity());
-		DummyEntity other = repository.save(createDummyEntity());
-
-		entity.getEmbeddable().setTest("something else");
-		other.getEmbeddable().setTest("others Name");
-
-		entity.getEmbeddable().getDummyEntity2().setTest("else");
-		other.getEmbeddable().getDummyEntity2().setTest("Name");
-
-		repository.saveAll(asList(entity, other));
-
-		assertThat(repository.findAll()) //
-				.extracting(d -> d.getEmbeddable().getTest()) //
-				.containsExactlyInAnyOrder(entity.getEmbeddable().getTest(), other.getEmbeddable().getTest());
-
-		assertThat(repository.findAll()) //
-				.extracting(d -> d.getEmbeddable().getDummyEntity2().getTest()) //
-				.containsExactlyInAnyOrder(entity.getEmbeddable().getDummyEntity2().getTest(),
-						other.getEmbeddable().getDummyEntity2().getTest());
-	}
-
-	@Test // DATAJDBC-111
-	public void deleteById() {
-
-		DummyEntity one = repository.save(createDummyEntity());
-		DummyEntity two = repository.save(createDummyEntity());
-		DummyEntity three = repository.save(createDummyEntity());
-
-		repository.deleteById(two.getId());
-
-		assertThat(repository.findAll()) //
-				.extracting(DummyEntity::getId) //
-				.containsExactlyInAnyOrder(one.getId(), three.getId());
-	}
-
-	@Test // DATAJDBC-111
-	public void deleteByEntity() {
-
-		DummyEntity one = repository.save(createDummyEntity());
-		DummyEntity two = repository.save(createDummyEntity());
-		DummyEntity three = repository.save(createDummyEntity());
-
-		repository.delete(one);
-
-		assertThat(repository.findAll()) //
-				.extracting(DummyEntity::getId) //
-				.containsExactlyInAnyOrder(two.getId(), three.getId());
-	}
-
-	@Test // DATAJDBC-111
-	public void deleteByList() {
-
-		DummyEntity one = repository.save(createDummyEntity());
-		DummyEntity two = repository.save(createDummyEntity());
-		DummyEntity three = repository.save(createDummyEntity());
-
-		repository.deleteAll(asList(one, three));
-
-		assertThat(repository.findAll()) //
-				.extracting(DummyEntity::getId) //
-				.containsExactlyInAnyOrder(two.getId());
-	}
-
-	@Test // DATAJDBC-111
-	public void deleteAll() {
-
-		repository.save(createDummyEntity());
-		repository.save(createDummyEntity());
-		repository.save(createDummyEntity());
-
-		assertThat(repository.findAll()).isNotEmpty();
-
-		repository.deleteAll();
-
-		assertThat(repository.findAll()).isEmpty();
-	}
-
-	@Test // DATAJDBC-318
-	public void queryDerivationLoadsReferencedEntitiesCorrectly() {
-
-		repository.save(createDummyEntity());
-		repository.save(createDummyEntity());
-		DummyEntity saved = repository.save(createDummyEntity());
-
-		assertThat(repository.findByTest(saved.test)) //
-				.extracting( //
-						e -> e.test, //
-						e -> e.embeddable.test, //
-						e -> e.embeddable.dummyEntity2.test //
-				).containsExactly( //
-						tuple(saved.test, saved.embeddable.test, saved.embeddable.dummyEntity2.test), //
-						tuple(saved.test, saved.embeddable.test, saved.embeddable.dummyEntity2.test), //
-						tuple(saved.test, saved.embeddable.test, saved.embeddable.dummyEntity2.test) //
-				);
-
-	}
 
 	interface DummyEntityRepository extends CrudRepository<DummyEntity, Long> {
 		List<DummyEntity> findByTest(String test);
