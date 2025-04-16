b60f28b846dc3826c4b79cfb88b1e523f12e4db4
Jens Schauder <jens.schauder@broadcom.com>
2025-04-16 14:09:37 +0200

    Polishing.

--- spring-data-relational/src/main/java/org/springframework/data/relational/core/sql/TupleExpression.java
+++ spring-data-relational/src/main/java/org/springframework/data/relational/core/sql/TupleExpression.java
@@ -1,6 +1,20 @@
-package org.springframework.data.relational.core.sql;
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import org.jetbrains.annotations.NotNull;
+package org.springframework.data.relational.core.sql;
 
 import static java.util.stream.Collectors.*;
 
@@ -8,7 +22,7 @@ import java.util.List;
 
 /**
  * A tuple as used in conditions like
- * 
+ *
  * <pre>
  *   WHERE (one, two) IN (select x, y from some_table)
  * </pre>
