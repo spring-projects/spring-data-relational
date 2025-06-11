1511e153139187db85b4ce6e0d8396ffc0706d92
Jens Schauder <jens.schauder@broadcom.com>
2025-06-11 11:42:57 +0200

    Polishing.
    Formatting.
    Simplification of code.
    
    Original pull request #2065
    See #2064

--- spring-data-jdbc/src/main/java/org/springframework/data/jdbc/core/JdbcAggregateTemplate.java
+++ spring-data-jdbc/src/main/java/org/springframework/data/jdbc/core/JdbcAggregateTemplate.java
@@ -28,7 +28,6 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-import org.jspecify.annotations.Nullable;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
@@ -51,22 +50,10 @@ import org.springframework.data.relational.core.conversion.RootAggregateChange;
 import org.springframework.data.relational.core.mapping.RelationalMappingContext;
 import org.springframework.data.relational.core.mapping.RelationalPersistentEntity;
 import org.springframework.data.relational.core.mapping.RelationalPersistentProperty;
-import org.springframework.data.relational.core.mapping.event.AbstractRelationalEvent;
-import org.springframework.data.relational.core.mapping.event.AfterConvertCallback;
-import org.springframework.data.relational.core.mapping.event.AfterConvertEvent;
-import org.springframework.data.relational.core.mapping.event.AfterDeleteCallback;
-import org.springframework.data.relational.core.mapping.event.AfterDeleteEvent;
-import org.springframework.data.relational.core.mapping.event.AfterSaveCallback;
-import org.springframework.data.relational.core.mapping.event.AfterSaveEvent;
-import org.springframework.data.relational.core.mapping.event.BeforeConvertCallback;
-import org.springframework.data.relational.core.mapping.event.BeforeConvertEvent;
-import org.springframework.data.relational.core.mapping.event.BeforeDeleteCallback;
-import org.springframework.data.relational.core.mapping.event.BeforeDeleteEvent;
-import org.springframework.data.relational.core.mapping.event.BeforeSaveCallback;
-import org.springframework.data.relational.core.mapping.event.BeforeSaveEvent;
-import org.springframework.data.relational.core.mapping.event.Identifier;
+import org.springframework.data.relational.core.mapping.event.*;
 import org.springframework.data.relational.core.query.Query;
 import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
 import org.springframework.util.ClassUtils;
 
@@ -82,6 +69,7 @@ import org.springframework.util.ClassUtils;
  * @author Chirag Tailor
  * @author Diego Krupitza
  * @author Sergey Korotaev
+ * @author Mikhail Polivakha
  */
 public class JdbcAggregateTemplate implements JdbcAggregateOperations {
 
@@ -708,16 +696,13 @@ public class JdbcAggregateTemplate implements JdbcAggregateOperations {
 		return null;
 	}
 
-	private record EntityAndPreviousVersion<T> (T entity, @Nullable Number version) {
+	private record EntityAndPreviousVersion<T>(T entity, @Nullable Number version) {
 	}
 
-	private record EntityAndChangeCreator<T> (T entity, AggregateChangeCreator<T> changeCreator) {
+	private record EntityAndChangeCreator<T>(T entity, AggregateChangeCreator<T> changeCreator) {
 	}
 
-	private interface AggregateChangeCreator<T> extends Function<T, RootAggregateChange<T>> {
-
-		default RootAggregateChange<T> createAggregateChange(T instance) {
-			return this.apply(instance);
-		}
+	private interface AggregateChangeCreator<T> {
+		RootAggregateChange<T> createAggregateChange(T instance);
 	}
 }
