dc8195f9c43c472e58932d1d64038ee357f008a3
Jens Schauder <jens.schauder@broadcom.com>
2025-10-22 11:05:13 +0200

    Deprecate methods accepting `Query` and `Pageable` returning `Page`.
    Since both the Query and the Pageable have limit, offset and sorting information, these methods are confusing and the strategy to resolve the conflict is not clear.
    
    Existing code using the methods got replaced.
    
    Users of these methods should use the various findAll and findBy methods to construct Page objects as desired.
    
    Closes #2138
    Original pull request #2161

--- spring-data-jdbc/src/main/java/org/springframework/data/jdbc/core/JdbcAggregateOperations.java
+++ spring-data-jdbc/src/main/java/org/springframework/data/jdbc/core/JdbcAggregateOperations.java
@@ -230,7 +230,9 @@ public interface JdbcAggregateOperations {
 	 * @param pageable the pagination information. Must not be {@code null}.
 	 * @return Guaranteed to be not {@code null}.
 	 * @since 2.0
+	 * @deprecated use a combination of other methods of this class to construct results of type {@link Page}.
 	 */
+	@Deprecated(since = "4.0")
 	<T> Page<T> findAll(Class<T> domainType, Pageable pageable);
 
 	/**
@@ -275,7 +277,9 @@ public interface JdbcAggregateOperations {
 	 * @param pageable can be null.
 	 * @return a {@link Page} of entities matching the given {@link Example}.
 	 * @since 3.0
+	 * @deprecated use a combination of other methods of this class to construct results of type {@link Page}.
 	 */
+	@Deprecated(since = "4.0")
 	<T> Page<T> findAll(Query query, Class<T> domainType, Pageable pageable);
 
 	/**
