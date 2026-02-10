b4477d0b46a8ba5fe07a1346a41bbb6286df0ee8
Christoph Strobl <christoph.strobl@broadcom.com>
2026-02-10 14:12:34 +0100

    Use String to store java.time MonthDay & YearMonth.
    Original Pull Request: #2184

--- spring-data-jdbc/src/test/java/org/springframework/data/jdbc/core/convert/JdbcColumnTypesUnitTests.java
+++ spring-data-jdbc/src/test/java/org/springframework/data/jdbc/core/convert/JdbcColumnTypesUnitTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 the original author or authors.
+ * Copyright 2025-present the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,21 +15,43 @@
  */
 package org.springframework.data.jdbc.core.convert;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.Month;
+import java.time.MonthDay;
+import java.time.OffsetTime;
 import java.time.Year;
+import java.time.YearMonth;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  * Unit tests for {@link JdbcColumnTypes}.
  *
  * @author Oliver Drotbohm
+ * @author Christoph Strobl
  */
 class JdbcColumnTypesUnitTests {
 
-	@Test
-	void usesIntegerForYear() {
-		assertThat(JdbcColumnTypes.INSTANCE.resolvePrimitiveType(Year.class)).isEqualTo(Integer.class);
+	@ParameterizedTest // GH-2184
+	@ValueSource(classes = { Year.class })
+	void usesIntegerFor(Class<?> type) {
+		assertThat(JdbcColumnTypes.INSTANCE.resolvePrimitiveType(type)).isEqualTo(Integer.class);
 	}
+
+	@ParameterizedTest // GH-2184
+	@ValueSource(classes = { Month.class, MonthDay.class, YearMonth.class })
+	void usesStringFor(Class<?> type) {
+		assertThat(JdbcColumnTypes.INSTANCE.resolvePrimitiveType(type)).isEqualTo(String.class);
+	}
+
+	@ParameterizedTest // GH-2184
+	@ValueSource(classes = { OffsetTime.class, Instant.class })
+	void usesTimestampFor(Class<?> type) {
+		assertThat(JdbcColumnTypes.INSTANCE.resolvePrimitiveType(type)).isEqualTo(Timestamp.class);
+	}
+
 }
