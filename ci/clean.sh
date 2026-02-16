9aa45556e8abdd992620ea2a01fecf276a60992a
Christoph Strobl <christoph.strobl@broadcom.com>
2026-02-16 11:50:34 +0100

    Remove obsolete CI configuration.
    See spring-projects/spring-data-build#2764

--- ci/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-
-set -euo pipefail
-
-export JENKINS_USER=${JENKINS_USER_NAME}
-
-MAVEN_OPTS="-Duser.name=${JENKINS_USER} -Duser.home=/tmp/jenkins-home" \
-  ./mvnw -s settings.xml -Dscan=false clean -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-jdbc -Ddevelocity.storage.directory=/tmp/jenkins-home/.develocity-root
