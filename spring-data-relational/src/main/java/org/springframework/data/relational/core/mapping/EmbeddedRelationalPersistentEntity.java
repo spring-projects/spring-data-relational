3f7753c24a096d0af09a5f591a19c7d6732d1ef8
Christoph Strobl <christoph.strobl@broadcom.com>
2026-03-06 14:42:33 +0100

    Adapt to changes in Spring Data Commons.
    See: spring-projects/spring-data-commons#2985

--- spring-data-relational/src/main/java/org/springframework/data/relational/core/mapping/EmbeddedRelationalPersistentEntity.java
+++ spring-data-relational/src/main/java/org/springframework/data/relational/core/mapping/EmbeddedRelationalPersistentEntity.java
@@ -161,6 +161,16 @@ class EmbeddedRelationalPersistentEntity<T> implements RelationalPersistentEntit
 	}
 
 	@Override
+	public @Nullable RelationalPersistentProperty getTransientProperty(String name) {
+		return delegate.getTransientProperty(name);
+	}
+
+	@Override
+	public boolean isTransient(String property) {
+		return delegate.isTransient(property);
+	}
+
+	@Override
 	public boolean hasIdProperty() {
 		return delegate.hasIdProperty();
 	}
